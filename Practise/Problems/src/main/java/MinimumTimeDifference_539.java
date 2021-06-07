39aa611f68e56dde59f6160251234b25aa4382e1
Sikander Singh <sikander441@gmail.com>
2021-06-07 12:37:24 +0530

    little optimization

--- Practise/Problems/src/main/java/MinimumTimeDifference_539.java
+++ Practise/Problems/src/main/java/MinimumTimeDifference_539.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -6,8 +7,9 @@ public class MinimumTimeDifference_539 {
     public int findMinDifference(List<String> timePoints) {
         boolean timeStamps[] = new boolean[ (24*60)+1];
         for(String timePoint: timePoints){
-            String t[] = timePoint.split(":");
-            int ti = Integer.parseInt(t[0])*60+Integer.parseInt(t[1]);
+            int h = (timePoint.charAt(0)-'0')*10+timePoint.charAt(1)-'0';
+            int m=(timePoint.charAt(3)-'0')*10+timePoint.charAt(4)-'0';
+            int ti = h*60+m;
             if(timeStamps[ti])
                 return 0;
             timeStamps[ti]=true;
@@ -16,10 +18,9 @@ public class MinimumTimeDifference_539 {
         int first=-1,prev=-1;
         for(int i=0;i<=24*60;i++){
             if(timeStamps[i]){
-                if(result==Integer.MAX_VALUE){
+                if(first==-1){
                   first = i;
                   prev = i;
-                  result = Integer.MAX_VALUE-1;
                   continue;
                 }
                 result = Math.min(result,i-prev);
